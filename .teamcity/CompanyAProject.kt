5f9187712c09a907ef56f99a9eed6a2668b55ff6
Esen Kubanychbekov <esen.kubanychbekov@financesoft.kg>
2026-03-02 14:57:10 +0600

    Change id

--- .teamcity/CompanyAProject.kt
+++ .teamcity/CompanyAProject.kt
@@ -2,7 +2,7 @@ import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.Project
 
 object CompanyAProject : Project({
-    id("Company A")
+    id("CompanyA")
     name = "Company A"
     buildType(CompanyABuild)
 })
